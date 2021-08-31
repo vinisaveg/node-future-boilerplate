e6856b895f5633ca88cc58d89bf05a9acc141cbe
savegnago <savegdesigner@gmail.com>
2021-08-31 18:45:10 -0300

    test: add supertest test example

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,17 @@
+import supertest from "supertest";
+
+import { app } from "../src/app";
+
 describe("Example test", () => {
   it("Should log Hello functional tests!", () => {
     const message = "Hello functional tests!";
 
     expect(message).toBe("Hello functional tests!");
+  });
+
+  it("Should get the Hello message from hello route", async () => {
+    const response = await supertest(app).get("/hello");
+
+    expect(response.body).toEqual({ message: "Hello world!" });
   });
 });
