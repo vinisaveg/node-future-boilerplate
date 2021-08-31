f6c47e0807f05e7d58a1f211d90318e26ce10eb8
savegnago <savegdesigner@gmail.com>
2021-08-30 21:54:46 -0300

    feat: add initial app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello world!!");
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/hello", (req, res) => {
+  return res.status(200).json({ message: "Hello world!" });
+});
+
+app.listen(4000, () => console.info("Server running on port 4000 🚀"));
+
+export { app };
